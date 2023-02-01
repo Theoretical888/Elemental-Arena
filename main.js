07be10b6086fb6695d03cd97254faa9e6222f587
William Fenton <w.fenton22@gmail.com>
2023-02-01 11:23:58 -0500

    Write function to get computer choice

--- main.js
+++ main.js
@@ -8,8 +8,21 @@ let playerScoreBoard = document.getElementById('footer-left');
 let enemeyScoreBoard = document.getElementById('footer-right');
 let playerWin = document.getElementById('player-win-alert');
 let tie = document.getElementById('tie-alert');
-let enemyWin = document.getEelemntById('enemy-win-alert');
+let enemyWin = document.getElementById('enemy-win-alert');
 let reset = document.getElementById('reset-button');
+
+function getEnemyChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if(randomNumber == 0) {
+        return 'fire';
+    } else if (randomNumber == 1) {
+        return 'water';
+    } else {
+        return 'earth';
+    } 
+}
+
+
 
 
 
