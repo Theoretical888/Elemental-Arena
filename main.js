3a322f6bd2f0af7bf881b0b7a000dee6790821b9
William Fenton <w.fenton22@gmail.com>
2023-02-01 16:11:58 -0500

    Add logic to function to alert who won the round, to add points to the scoreboard, and change the scoreboard border color based on who won the round.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let pScore = document.getElementById('player-score');
 let eScore = document. getElementById('enemy-score')
 let enemyChoice = document.getElementById('enemy-choice');
 let playerScoreBoard = document.getElementById('footer-left');
-let enemeyScoreBoard = document.getElementById('footer-right');
+let enemyScoreBoard = document.getElementById('footer-right');
 let playerWin = document.getElementById('player-win-alert');
 let tie = document.getElementById('tie-alert');
 let enemyWin = document.getElementById('enemy-win-alert');
@@ -21,8 +21,8 @@ function getEnemyChoice() {
         return 'water';
     } else {
         return 'earth';
-    } 
-}
+    };
+};
 
 function startGame() {
     elements.forEach((element) => 
@@ -30,20 +30,21 @@ function startGame() {
         playRound(element.id);
     })
   );
-}
+};
 
 function checkScore() {
     // create a function to check the score after each round
-}
+};
 
 function resetGame() {
     // create a function to reset the game once the reset button is pressed
-}
+};
 
 function playRound(playerSelection) {
     let enemySelection = getEnemyChoice();
     enemyChoice.classList.remove('fa-solid', 'fa-brands', 'fa-question', 
             'fa-fire-flame-curved', 'fa-droplet', 'fa-envira');
+
     if (enemySelection === 'fire') {
             enemyChoice.classList.add('fa-solid', 'fa-fire-flame-curved');
     } else if (enemySelection === 'water') {
@@ -51,8 +52,66 @@ function playRound(playerSelection) {
     } else if(enemySelection === 'earth') {
         enemyChoice.classList.add('fa-brands', 'fa-envira');
     } else {
-        enemyChoice.classList.add('fa-solid', 'fa-question')
-    }
+        enemyChoice.classList.add('fa-solid', 'fa-question');
+    };
+
+    if (playerSelection === enemySelection) {
+        playerWin.innerText = '';
+        enemyWin.innerText = '';
+        tie.innerText = 'It\s a tie';
+        playerScoreBoard.style.border = '4px solid var(--off-white)';
+        enemyScoreBoard.style.border = '4px solid var(--off-white)';
+    } else if (playerSelection === 'fire' && enemySelection === 'earth') {
+        tie.innerText = '';
+        enemyWin.innerText = '';
+        playerWin.innerText = 'Fire incinerates earth!'
+        playerScore++;
+        pScore.innerText = `Player: ${playerScore}`;
+        playerScoreBoard.style.border = '4px solid var(--green)';
+        enemyScoreBoard.style.border = '4px solid var(--red)';
+    } else if (playerSelection === 'fire' && enemySelection === 'water') {
+        tie.innerText = '';
+        playerWin.innerText = '';
+        enemyWin.innerText = 'Water extinguishes fire!'
+        enemyScore++;
+        eScore.innerText = `${enemyScore} :Enemy`;
+        playerScoreBoard.style.border = '4px solid var(--red)';
+        enemyScoreBoard.style.border = '4px solid var(--green)';
+    } else if (playerSelection === 'water' && enemySelection === 'fire') {
+        tie.innerText = '';
+        enemyWin.innerText = '';
+        playerWin.innerText = 'Water extinguishes fire!';
+        playerScore++;
+        pScore.innerText = `Player: ${playerScore}`;
+        playerScoreBoard.style.border = '4px solid var(--green)';
+        enemyScoreBoard.style.border = '4px solid var(--red)';
+    } else if (playerSelection === 'water' && enemySelection === 'earth') {
+        tie.innerText = '';
+        playerWin.innerText = '';
+        enemyWin.innerText = 'Earth overpowers water!';
+        enemyScore++;
+        eScore.innerText = `${enemyScore} :Enemy`;
+        playerScoreBoard.style.border = '4px solid var(--red)';
+        enemyScoreBoard.style.border = '4px solid var(--green)';
+    } else if (playerSelection === 'earth' && enemySelection === 'water') {
+        tie.innerText = '';
+        enemyWin.innerText = '';
+        playerWin.innerText = 'Earth overpowers water!';
+        playerScore++;
+        pScore.innerText = `Player: ${playerScore}`;
+        playerScoreBoard.style.border = '4px solid var(--green)';
+        enemyScoreBoard.style.border = '4px solid var(--red)';
+    } else if (playerSelection === 'earth' && enemySelection === 'fire') {
+        tie.innerText = '';
+        playerWin.innerText = '';
+        enemyWin.innerText = 'Fire incinerates earth!';
+        enemyScore++;
+        eScore.innerText = `${enemyScore} :Enemy`;
+        playerScoreBoard.style.border = '4px solid var(--red)';
+        enemyScoreBoard.style.border = '4px solid var(--green)';
+    } 
+
+
 }
 
 startGame();
