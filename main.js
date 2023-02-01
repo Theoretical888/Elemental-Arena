0d6c5a2dd58cf01142b26cee1fca9bb703f64cc2
William Fenton <w.fenton22@gmail.com>
2023-02-01 15:22:25 -0500

    Implement logic to display the icon of the enemy selection within its respective div.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ elements = Array.from(elements)
 
 let playerScore = 0;
 let enemyScore = 0;
+let pScore = document.getElementById('player-score');
+let eScore = document. getElementById('enemy-score')
 let enemyChoice = document.getElementById('enemy-choice');
 let playerScoreBoard = document.getElementById('footer-left');
 let enemeyScoreBoard = document.getElementById('footer-right');
@@ -25,9 +27,35 @@ function getEnemyChoice() {
 function startGame() {
     elements.forEach((element) => 
     element.addEventListener('click', () => {
-        playRound(element.id)
-    }))
+        playRound(element.id);
+    })
+  );
 }
+
+function checkScore() {
+    // create a function to check the score after each round
+}
+
+function resetGame() {
+    // create a function to reset the game once the reset button is pressed
+}
+
+function playRound(playerSelection) {
+    let enemySelection = getEnemyChoice();
+    enemyChoice.classList.remove('fa-solid', 'fa-brands', 'fa-question', 
+            'fa-fire-flame-curved', 'fa-droplet', 'fa-envira');
+    if (enemySelection === 'fire') {
+            enemyChoice.classList.add('fa-solid', 'fa-fire-flame-curved');
+    } else if (enemySelection === 'water') {
+        enemyChoice.classList.add('fa-solid', 'fa-droplet');
+    } else {
+        enemyChoice.classList.add('fa-brands', 'fa-envira');
+    }
+}
+
+startGame();
+
+
 
 
 
