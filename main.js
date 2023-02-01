1dd2e596c9e5e369b05055a06a904635c4f44f67
William Fenton <w.fenton22@gmail.com>
2023-02-01 10:53:16 -0500

    Make HTML structure, style page and add responsiveness. Declare global variables of things you will need to manipulate. Forget to commit throughout entire process.

---
+++ main.js
@@ -0,0 +1,21 @@
+let elements = document.getElementsByClassName('element');
+elements = Array.from(elements)
+
+let playerScore = 0;
+let enemyScore = 0;
+let enemyChoice = document.getElementById('enemy-choice');
+let playerScoreBoard = document.getElementById('footer-left');
+let enemeyScoreBoard = document.getElementById('footer-right');
+let playerWin = document.getElementById('player-win-alert');
+let tie = document.getElementById('tie-alert');
+let enemyWin = document.getEelemntById('enemy-win-alert');
+let reset = document.getElementById('reset-button');
+
+
+
+
+
+
+
+
+
